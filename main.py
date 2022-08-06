e06848a2646e3e5fa4d87c310ea928958a067c27
Timothy Cloninger <timothy.cloninger@life.church>
2022-08-05 20:47:02 -0500

    Add line-of-sight

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ MEMORY_LENGTH = 8
 # TODO: re:build_maze, prefer neighboring cells that have fewer visited neighbors
 # TODO: start small, finishing opens a harder one
 # TODO: add random connections to add possibility of looping?
-# TODO: show line-of-sight cells
+# TODO: cleanup line-of-sight history
 
 
 maze.build_grid()
@@ -37,16 +37,16 @@ while RUN:
             if event.key == pygame.K_ESCAPE:
                 RUN = False
                 break
-            elif event.key == pygame.K_RIGHT and (idx[0] + 1, idx[1]) in grid[idx].connections:
+            elif event.key == pygame.K_RIGHT and (idx[0] + 1, idx[1]) in grid[idx].links.values():
                 history = [idx] + history
                 idx = (idx[0] + 1, idx[1])
-            elif event.key == pygame.K_LEFT and (idx[0] - 1, idx[1]) in grid[idx].connections:
+            elif event.key == pygame.K_LEFT and (idx[0] - 1, idx[1]) in grid[idx].links.values():
                 history = [idx] + history
                 idx = (idx[0] - 1, idx[1])
-            elif event.key == pygame.K_DOWN and (idx[0], idx[1] + 1) in grid[idx].connections:
+            elif event.key == pygame.K_DOWN and (idx[0], idx[1] + 1) in grid[idx].links.values():
                 history = [idx] + history
                 idx = (idx[0], idx[1] + 1)
-            elif event.key == pygame.K_UP and (idx[0], idx[1] - 1) in grid[idx].connections:
+            elif event.key == pygame.K_UP and (idx[0], idx[1] - 1) in grid[idx].links.values():
                 history = [idx] + history
                 idx = (idx[0], idx[1] - 1)
 
@@ -58,6 +58,13 @@ while RUN:
                 history = history[:MEMORY_LENGTH]
 
             grid[idx].show_walls()
+
+            # line of sight
+            for d, coord in grid[idx].links.items():
+                while coord:
+                    grid[coord].show_walls()
+                    coord = grid[coord].links.get(d)
+
             grid[idx].show_user()
             pygame.display.update()
 
--- maze.py
+++ maze.py
@@ -1,6 +1,4 @@
-import pdb
 import random
-from tkinter.tix import CELL
 import pygame
 
 BLACK = (0, 0, 0)
@@ -11,13 +9,13 @@ RED = (255, 0, 0)
 GREY = (235, 235, 235)
 
 MARGIN = 10
-CELL_WIDTH = 40
-WIDTH, HEIGHT = 30, 20
+CELL_WIDTH = 20
+WIDTH, HEIGHT = 24, 16
 DISPLAY_SIZE = (
     WIDTH * CELL_WIDTH + MARGIN * 2,
     HEIGHT * CELL_WIDTH + MARGIN * 2
 )
-TRACER_WIDTH = 12
+TRACER_WIDTH = 8
 FPS = 30
 
 
@@ -32,27 +30,19 @@ pygame.display.update()
 
 grid = {}
 path = {}
-connections = []
 
 
 class Point:
     def __init__(self, i, j):
         self.i = i
         self.j = j
-        self.possible_connections = set(
-            [
-                (i + 1, j),
-                (i - 1, j),
-                (i, j + 1),
-                (i, j - 1),
-            ]
-        )
-
-        self.connections = set()
-
-    @property
-    def walls(self):
-        return self.possible_connections.difference(self.connections)
+        self.directions = {
+            (i + 1, j): "east",
+            (i - 1, j): "west",
+            (i, j + 1): "south",
+            (i, j - 1): "north",
+        }
+        self.links = {}
 
     def _to_tracer(self, coord):
         return coord * CELL_WIDTH + MARGIN + (CELL_WIDTH - TRACER_WIDTH) // 2
@@ -70,32 +60,15 @@ class Point:
     def show_walls(self):
         cw = CELL_WIDTH
         pygame.draw.rect(win, BLACK, (self.i * cw + MARGIN, self.j * cw + MARGIN, cw, cw), 0) # walls
-        pygame.draw.rect(win, WHITE, (self.i * cw + MARGIN + 1, self.j * cw + MARGIN + 1, cw - 2, cw - 2), 0) # walls
-        for c in self.connections:
-            midway = (self.i + c[0]) / 2, (self.j + c[1]) / 2
+        pygame.draw.rect(win, WHITE, (self.i * cw + MARGIN + 1, self.j * cw + MARGIN + 1, cw - 2, cw - 2), 0) # knockout
+        for i, j in self.links.values():
+            midway = (self.i + i) / 2, (self.j + j) / 2
             pygame.draw.rect(win, WHITE, (midway[0] * cw + MARGIN + 1, midway[1] * cw + MARGIN + 1, cw - 2, cw - 2), 0)
 
 
 def build_grid(width=WIDTH, height=HEIGHT, cw=CELL_WIDTH):
     global grid
     grid = {(i, j): Point(i, j) for j in range(height) for i in range(width)}
-
-    # for j in range(height):
-    #     y = cw * j + MARGIN
-    #     for i in range(width):
-    #         x = cw * i + MARGIN
-
-    #         pygame.draw.line(win, BLACK, [x, y], [x + cw, y], 2) # North wall
-    #         pygame.draw.line(win, BLACK, [x, y], [x, y + cw], 2) # West wall
-    #         pygame.draw.line(win, BLACK, [x + cw, y], [x + cw, y + cw], 2) # East wall
-    #         pygame.draw.line(win, BLACK, [x, y + cw], [x + cw, y + cw], 2) # South wall
-
-    # #         # grid.append((i, j))
-
-
-def knockdown_cell(coord):
-    cw = CELL_WIDTH
-    pygame.draw.rect(win, WHITE, (coord[0] * cw + MARGIN + 1, coord[1] * cw + MARGIN + 1, cw - 1, cw - 1), 0)
 
 
 def build_maze():
@@ -106,25 +79,29 @@ def build_maze():
     stack_list = []
     visited_cells = []
 
-    current = 0, 0
-    stack_list.append(current)
-    visited_cells.append(current)
+    idx = 0, 0
+    stack_list.append(idx)
+    visited_cells.append(idx)
 
     while stack_list:
-        # knockdown_cell(current)
-        all_directions = grid[current].possible_connections
-        valid_directions = [d for d in all_directions if _valid_direction(d)]
+        valid_directions = [d for d in grid[idx].directions if _valid_direction(d)]
 
         if valid_directions:
-            selected = random.choice(valid_directions)
-            # midway = (current[0] + selected[0]) / 2, (current[1] + selected[1]) / 2
-            # knockdown_cell(midway)
-            path[selected] = current
-            visited_cells.append(selected)
-            stack_list.append(selected)
-            grid[current].connections.add(selected)
-            grid[selected].connections.add(current)
-            current = selected
+            new_idx = random.choice(valid_directions)
+
+            path[new_idx] = idx  # do I need this?
+            visited_cells.append(new_idx)
+            stack_list.append(new_idx)
+
+            # link from idx to new_idx
+            absolute_direction = grid[idx].directions[new_idx]
+            grid[idx].links[absolute_direction] = new_idx
+
+            # link from new_idx to idx
+            absolute_direction = grid[new_idx].directions[idx]
+            grid[new_idx].links[absolute_direction] = idx
+
+            idx = new_idx
 
         else:
-            current = stack_list.pop()
+            idx = stack_list.pop()
